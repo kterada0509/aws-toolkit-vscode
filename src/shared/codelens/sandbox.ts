017ce4fd00f605968d1a0a4c6f88d71174763f8e
Matthew Pirocchi <pirocchi@amazon.com>
2019-04-11 20:35:57 -0700

    Waiting on Output Watching and Run Command

--- src/shared/codelens/sandbox.ts
+++ src/shared/codelens/sandbox.ts
@@ -10,6 +10,7 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as util from 'util'
 import * as vscode from 'vscode'
+import { CloudFormation } from '../cloudformation/cloudformation'
 import { SamCliBuildInvocation } from '../sam/cli/samCliBuild'
 import { DefaultSamCliProcessInvoker } from '../sam/cli/samCliInvoker'
 import { SamCliProcessInvoker } from '../sam/cli/samCliInvokerUtils'
@@ -24,7 +25,7 @@ interface PipeTransport {
     pipeCwd: string
 }
 
-interface DotNetDebugConfiguration extends vscode.DebugConfiguration {
+interface DotNetCoreDebugConfiguration extends vscode.DebugConfiguration {
     type: 'coreclr'
     request: 'attach'
     processId: string
@@ -37,36 +38,38 @@ interface DotNetDebugConfiguration extends vscode.DebugConfiguration {
     }
 }
 
-interface StartDebuggingContext {
+interface DebugHandlerContext {
     startDebugging: typeof vscode.debug.startDebugging
     invoker: SamCliProcessInvoker
+    loadTemplate: typeof CloudFormation.load
 }
 
-interface StartDebuggingArgs {
-    runtime: 'dotnetcore2.0' | 'dotnetcore2.1',
-    codeUri: vscode.Uri,
-    samAppRoot: vscode.Uri
+interface DebugHandlerArgs {
     port: number,
     workspaceFolder: vscode.WorkspaceFolder,
-    templatePath: string
+    templatePath: string,
+    handlerName: string
 }
 
-export async function startDebugging(
-    args: StartDebuggingArgs,
-    context: StartDebuggingContext = {
+export async function debugHandler(
+    args: DebugHandlerArgs,
+    context: DebugHandlerContext = {
         startDebugging: vscode.debug.startDebugging,
-        invoker: new DefaultSamCliProcessInvoker()
+        invoker: new DefaultSamCliProcessInvoker(),
+        loadTemplate: CloudFormation.load
     }
 ): Promise<void> {
+    const resource = await getResourceFromTemplate(args, context)
+
     await build(args, context)
-    await installDebugger(args)
+    await installDebugger(resource)
     await invoke()
-    await attach(args, context)
+    await attach({ ...args, resource }, context)
 }
 
 async function build(
-    { templatePath }: Pick<StartDebuggingArgs, 'templatePath'>,
-    { invoker }: Pick<StartDebuggingContext, 'invoker'>
+    { templatePath }: Pick<DebugHandlerArgs, 'templatePath'>,
+    { invoker }: Pick<DebugHandlerContext, 'invoker'>
 ): Promise<void> {
     const buildDir = '' // TODO
     const invocation = new SamCliBuildInvocation({
@@ -82,40 +85,50 @@ async function build(
     await invocation.execute()
 }
 
-async function installDebugger(
-    { runtime, codeUri }: Pick<StartDebuggingArgs, 'codeUri' | 'runtime'>
-): Promise<void> {
-    const vsdbgPath = path.resolve(codeUri.fsPath, '.vsdbg')
+async function installDebugger(resource: CloudFormation.Resource): Promise<void> {
+    const runtime = getRuntime(resource)
+    const codeUri = getCodeUri(resource)
+    const vsdbgPath = path.resolve(codeUri, '.vsdbg')
+
     try {
         await access(vsdbgPath)
+
+        // vsdbg is already installed.
+        return
     } catch {
-        await mkdir(vsdbgPath)
+        // We could not access vsdbgPath. Swallow error and continue.
     }
 
-    const process = crossSpawn(
-        'docker',
-        [
-            'run',
-            '--rm',
-            '--mount',
-            `type=bind,src=${vsdbgPath},dst=/vsdbg`,
-            '--entrypoint',
-            'bash',
-            `lambci/lambda:${runtime}`,
-            '-c',
-            '"curl -sSL https://aka.ms/getvsdbgsh | bash /dev/stdin -v latest -l /vsdbg"'
-        ],
-    )
+    try {
+        await mkdir(vsdbgPath)
 
-    await new Promise<void>((resolve, reject) => {
-        process.once('close', (code, signal) => {
-            if (code === 0) {
-                resolve()
-            } else {
-                reject(signal)
-            }
+        const process = crossSpawn(
+            'docker',
+            [
+                'run',
+                '--rm',
+                '--mount',
+                `type=bind,src=${vsdbgPath},dst=/vsdbg`,
+                '--entrypoint',
+                'bash',
+                `lambci/lambda:${runtime}`,
+                '-c',
+                '"curl -sSL https://aka.ms/getvsdbgsh | bash /dev/stdin -v latest -l /vsdbg"'
+            ],
+        )
+
+        await new Promise<void>((resolve, reject) => {
+            process.once('close', (code, signal) => {
+                if (code === 0) {
+                    resolve()
+                } else {
+                    reject(signal)
+                }
+            })
         })
-    })
+    } catch {
+        // TODO: rm -rf vsdbgPath
+    }
 }
 
 async function invoke() {
@@ -125,22 +138,22 @@ async function invoke() {
 }
 
 async function attach(
-    args: Pick<StartDebuggingArgs, 'codeUri' | 'port' | 'workspaceFolder'>,
-    context: StartDebuggingContext
+    args: Pick<DebugHandlerArgs, 'port' | 'workspaceFolder'> & { resource: CloudFormation.Resource },
+    context: DebugHandlerContext
 ) {
     const config = makeDebugConfiguration(args)
     await context.startDebugging(args.workspaceFolder, config)
 }
 
 function makeDebugConfiguration(
-    { port, codeUri }: Pick<StartDebuggingArgs, 'port' | 'codeUri'>
-): DotNetDebugConfiguration {
+    { port, resource }: Pick<DebugHandlerArgs, 'port'> & { resource: CloudFormation.Resource }
+): DotNetCoreDebugConfiguration {
     const pipeArgs = [
         '-c',
         `docker exec -i $(docker ps -q -f publish=${port}) \${debuggerCommand}`
     ]
     const debuggerPath = '/tmp/lambci_debug_files/vsdbg'
-    const pipeCwd = codeUri.fsPath
+    const codeUri = getCodeUri(resource)
 
     return {
         name: '.NET Core Docker Attach',
@@ -151,18 +164,63 @@ function makeDebugConfiguration(
             pipeProgram: 'sh',
             pipeArgs,
             debuggerPath,
-            pipeCwd
+            pipeCwd: codeUri
         },
         windows: {
             pipeTransport: {
                 pipeProgram: 'powershell',
                 pipeArgs,
                 debuggerPath,
-                pipeCwd
+                pipeCwd: codeUri
             }
         },
         sourceFileMap: {
-            ['/var/task']: codeUri.fsPath
+            ['/var/task']: codeUri
         }
     }
+}
+
+function getRuntime(resource: CloudFormation.Resource): string {
+    if (!resource.Properties || !resource.Properties.Runtime) {
+        throw new Error('Resource does not specify a Runtime')
+    }
+
+    return resource.Properties!.Runtime!
+}
+
+function getCodeUri(resource: CloudFormation.Resource): string {
+    if (!resource.Properties || !resource.Properties.CodeUri) {
+        throw new Error('Resource does not specify a CodeUri')
+    }
+
+    return resource.Properties!.CodeUri
+}
+
+async function getResourceFromTemplate(
+    { templatePath, handlerName }: Pick<DebugHandlerArgs, 'templatePath' | 'handlerName'>,
+    context: Pick<DebugHandlerContext, 'loadTemplate'>
+): Promise<CloudFormation.Resource> {
+    const template = await context.loadTemplate(templatePath)
+
+    if (!template.Resources) {
+        throw new Error(`Could not find a SAM resource for handler ${handlerName}`)
+    }
+
+    const resources = template.Resources
+    const matches = Object.keys(resources)
+        .filter(key =>
+            !!resources[key] && resources[key]!.Type === 'AWS::Serverless::Function' &&
+            !!resources[key]!.Properties && resources[key]!.Properties!.Handler === handlerName
+        ).map(key => resources[key]!)
+
+    if (matches.length < 1) {
+        throw new Error(`Could not find a SAM resource for handler ${handlerName}`)
+    }
+
+    if (matches.length > 1) {
+        // TODO: Is this a valid scenario? It won't be once we're template-first.
+        throw new Error(`Found more than one SAM resource for handler ${handlerName}`)
+    }
+
+    return matches[0]
 }
